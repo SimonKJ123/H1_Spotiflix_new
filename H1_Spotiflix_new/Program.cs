2a2ca2f98e5a1d45e56977f9006bd2314b9b99ad
sk <sk@PROELE82>
2023-08-21 09:43:34 +0200

    Calling methods instead of having them inside the cases

--- H1_Spotiflix_new/Program.cs
+++ H1_Spotiflix_new/Program.cs
@@ -52,224 +52,52 @@ while (true)
     {
         case "1":
             {
-                Console.Clear();
-                Console.WriteLine("Info about Movie");
-                Console.Write("Title: ");
-                string title = Console.ReadLine();
-                Console.Write("Length: ");
-                string Length = Console.ReadLine();
-                Console.Write("Gerne: ");
-                string Gerne = Console.ReadLine();
-                Console.Write("Release Date:");
-                string ReleaseD = Console.ReadLine();
-                Console.Write("Website:");
-                string Web = Console.ReadLine();
-
-                database.Movies.Add(new Movie
-                {
-                    Title = title,
-                    Length = Length,
-                    Genre = Gerne,
-                    Relasedate = ReleaseD,
-                    Web = Web,
-                });
-                Save(database);
-                Console.WriteLine("Movie added to DataBase");
-                Console.ReadKey();
+                InfoAboutMovie(path, database);
                 break;
             }
         case "2":
             {
-                Console.Clear();
-                Console.WriteLine("List of Movies in DataBase");
-
-                foreach (var movie in database.Movies)
-                {
-                    movie.Print();
-                }
-
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                MovieDatabase(database);
                 break;
             }
         case "3":
             {
-                Console.Clear();
-                Console.Write("Query: ");
-                string query = Console.ReadLine();
-                foreach (var movie in database.Movies)
-                {
-                    if (movie.Contains(query))
-                    {
-                        movie.Print();
-                        
-                    }
-                }
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                PrintMovie(database);
                 break;
             }
         case "4":
             {
-                Console.Clear();
-                Console.WriteLine("Info about Series");
-                Console.Write("Title: ");
-                string title = Console.ReadLine();
-                Console.Write("Length: ");
-                string Length = Console.ReadLine();
-                Console.Write("Gerne: ");
-                string Gerne = Console.ReadLine();
-                Console.Write("Release Date: ");
-                string ReleaseD = Console.ReadLine();
-                Console.Write("Episode: ");
-                string ep = Console.ReadLine();
-                Console.Write("Seasons: ");
-                string seasons = Console.ReadLine();
-                Console.Write("Episode title: ");
-                string ept = Console.ReadLine();
-                Console.Write("Website: ");
-                string Web = Console.ReadLine();
-
-                database.Seriess.Add(new Series
-                {
-                    Title = title,
-                    Episode = ep,
-                    TitleEpisode = ept,
-                    Length = Length,
-                    Season = seasons,
-                    Genre = Gerne,
-                    Relasedate = ReleaseD,
-                    Web = Web,
-                });
-                Save(database);
-
-                Console.WriteLine("Series added to DataBase");
-                Console.ReadKey();
+                CreateSerieslist(path, database);
                 break;
             }
         case "5":
             {
-                Console.Clear();
-                Console.WriteLine("List of Series in DataBase");
-
-                foreach (var series in database.Seriess)
-                {
-                    series.Print();
-                }
-
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                SeriesDatabase(database);
                 break;
             }
         case "6":
             {
-                Console.Clear();
-                Console.Write("Query: ");
-                string query = Console.ReadLine();
-                foreach (var series in database.Seriess)
-                {
-                    if (series.Contains(query))
-                    {
-                        series.Print();
-                    }
-                }
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                PrintSeries(database);
                 break;
             }
         case "7":
             {
-                Console.Clear();
-                Console.WriteLine("Info about Song");
-                Console.Write("Title: ");
-                string title = Console.ReadLine();
-                Console.Write("Length: ");
-                string Length = Console.ReadLine();
-                Console.Write("Gerne: ");
-                string Gerne = Console.ReadLine();
-                Console.Write("Release Date: ");
-                string ReleaseD = Console.ReadLine();
-                Console.Write("Arist: ");
-                string Artist = Console.ReadLine();
-                Console.Write("Album: ");
-                string Album = Console.ReadLine();
-                Console.Write("Website: ");
-                string Web = Console.ReadLine();
-
-                database.Musics.Add(new Music
-                {
-                    Title = title,
-                    Length = Length,
-                    Genre = Gerne,
-                    Relasedate = ReleaseD,
-                    Artist = Artist,
-                    Album = Album,
-                    Web = Web,
-                });
-                Save(database);
-                Console.WriteLine("Song added to DataBase");
-                Console.ReadKey();
+                InfoAboutSong(path, database);
                 break;
             }
         case "8":
             {
-                Console.Clear();
-                Console.WriteLine("List of Music in DataBase");
-
-                foreach (var music in database.Musics)
-                {
-                    music.Print();
-                }
-
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                SongDatabase(database);
                 break;
             }
         case "9":
             {
-                Console.Clear();
-                Console.Write("Query: ");
-                string query = Console.ReadLine();
-                foreach (var music in database.Musics)
-                {
-                    if (music.Contains(query))
-                    {
-                        music.Print();
-                    }
-                }
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                PrintSong(database);
                 break;
             }
         case "10":
             {
-                Console.Clear();
-                Console.Write("Query: ");
-                string query = Console.ReadLine();
-                foreach (var movie in database.Movies)
-                {
-                    if (movie.Contains(query))
-                    {
-                        movie.Print();
-
-                    }
-                }
-                foreach (var series in database.Seriess)
-                {
-                    if (series.Contains(query))
-                    {
-                        series.Print();
-                    }
-                }
-                foreach (var music in database.Musics)
-                {
-                    if (music.Contains(query))
-                    {
-                        music.Print();
-                    }
-                }
-                Console.WriteLine("Press any key to return");
-                Console.ReadKey();
+                SeriesInfo(database);
 
                 break;
             }
@@ -286,4 +114,226 @@ while (true)
                 break;
             }
     }
+}
+
+void InfoAboutMovie(string path, Database database)
+{
+    Console.Clear();
+    Console.WriteLine("Info about Movie");
+    Console.Write("Title: ");
+    string title = Console.ReadLine();
+    Console.Write("Length: ");
+    string Length = Console.ReadLine();
+    Console.Write("Gerne: ");
+    string Gerne = Console.ReadLine();
+    Console.Write("Release Date:");
+    string ReleaseD = Console.ReadLine();
+    Console.Write("Website:");
+    string Web = Console.ReadLine();
+
+    database.Movies.Add(new Movie
+    {
+        Title = title,
+        Length = Length,
+        Genre = Gerne,
+        Relasedate = ReleaseD,
+        Web = Web,
+    });
+    Save(database);
+    Console.WriteLine("Movie added to DataBase");
+    Console.ReadKey();
+}
+
+void MovieDatabase(Database database)
+{
+    Console.Clear();
+    Console.WriteLine("List of Movies in DataBase");
+
+    foreach (var movie in database.Movies)
+    {
+        movie.Print();
+    }
+
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void PrintMovie(Database database)
+{
+    Console.Clear();
+    Console.Write("Query: ");
+    string query = Console.ReadLine();
+    foreach (var movie in database.Movies)
+    {
+        if (movie.Contains(query))
+        {
+            movie.Print();
+
+        }
+    }
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void CreateSerieslist(string path, Database database)
+{
+    Console.Clear();
+    Console.WriteLine("Info about Series");
+    Console.Write("Title: ");
+    string title = Console.ReadLine();
+    Console.Write("Length: ");
+    string Length = Console.ReadLine();
+    Console.Write("Gerne: ");
+    string Gerne = Console.ReadLine();
+    Console.Write("Release Date: ");
+    string ReleaseD = Console.ReadLine();
+    Console.Write("Episode: ");
+    string ep = Console.ReadLine();
+    Console.Write("Seasons: ");
+    string seasons = Console.ReadLine();
+    Console.Write("Episode title: ");
+    string ept = Console.ReadLine();
+    Console.Write("Website: ");
+    string Web = Console.ReadLine();
+
+    database.Seriess.Add(new Series
+    {
+        Title = title,
+        Episode = ep,
+        TitleEpisode = ept,
+        Length = Length,
+        Season = seasons,
+        Genre = Gerne,
+        Relasedate = ReleaseD,
+        Web = Web,
+    });
+    Save(database);
+
+    Console.WriteLine("Series added to DataBase");
+    Console.ReadKey();
+}
+
+void SeriesDatabase(Database database)
+{
+    Console.Clear();
+    Console.WriteLine("List of Series in DataBase");
+
+    foreach (var series in database.Seriess)
+    {
+        series.Print();
+    }
+
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void PrintSeries(Database database)
+{
+    Console.Clear();
+    Console.Write("Query: ");
+    string query = Console.ReadLine();
+    foreach (var series in database.Seriess)
+    {
+        if (series.Contains(query))
+        {
+            series.Print();
+        }
+    }
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void InfoAboutSong(string path, Database database)
+{
+    Console.Clear();
+    Console.WriteLine("Info about Song");
+    Console.Write("Title: ");
+    string title = Console.ReadLine();
+    Console.Write("Length: ");
+    string Length = Console.ReadLine();
+    Console.Write("Gerne: ");
+    string Gerne = Console.ReadLine();
+    Console.Write("Release Date: ");
+    string ReleaseD = Console.ReadLine();
+    Console.Write("Arist: ");
+    string Artist = Console.ReadLine();
+    Console.Write("Album: ");
+    string Album = Console.ReadLine();
+    Console.Write("Website: ");
+    string Web = Console.ReadLine();
+
+    database.Musics.Add(new Music
+    {
+        Title = title,
+        Length = Length,
+        Genre = Gerne,
+        Relasedate = ReleaseD,
+        Artist = Artist,
+        Album = Album,
+        Web = Web,
+    });
+    Save(database);
+    Console.WriteLine("Song added to DataBase");
+    Console.ReadKey();
+}
+
+void SongDatabase(Database database)
+{
+    Console.Clear();
+    Console.WriteLine("List of Music in DataBase");
+
+    foreach (var music in database.Musics)
+    {
+        music.Print();
+    }
+
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void PrintSong(Database database)
+{
+    Console.Clear();
+    Console.Write("Query: ");
+    string query = Console.ReadLine();
+    foreach (var music in database.Musics)
+    {
+        if (music.Contains(query))
+        {
+            music.Print();
+        }
+    }
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
+}
+
+void SeriesInfo(Database database)
+{
+    Console.Clear();
+    Console.Write("Query: ");
+    string query = Console.ReadLine();
+    foreach (var movie in database.Movies)
+    {
+        if (movie.Contains(query))
+        {
+            movie.Print();
+
+        }
+    }
+    foreach (var series in database.Seriess)
+    {
+        if (series.Contains(query))
+        {
+            series.Print();
+        }
+    }
+    foreach (var music in database.Musics)
+    {
+        if (music.Contains(query))
+        {
+            music.Print();
+        }
+    }
+    Console.WriteLine("Press any key to return");
+    Console.ReadKey();
 }
